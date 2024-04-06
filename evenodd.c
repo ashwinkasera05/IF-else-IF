a7e965c42c04696c831ec41fca98199496c5b5cc
ashwinkasera05 <ashwintamrakar09>
2024-04-07 01:41:36 +0530

    new file added

---
+++ evenodd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int num;
+    printf("enter any no:");
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+        printf("%d is even no.",num);
+    }
+    else
+    {
+        printf("%d is odd  no.",num);
+    }
+}
